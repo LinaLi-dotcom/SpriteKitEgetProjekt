b81d9b7da2931fac2fe8eb0b4fe07e8a5e0f5fdb
lina <linasigridli@gmail.com>
2020-12-14 09:30:48 +0100

    Initial Commit

---
+++ SpriteKitEgetProjekt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpriteKitEgetProjekt
+//
+//  Created by Lina Li on 2020-12-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpriteKitEgetProjekt/SpriteKitEgetProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitEgetProjektApp.swift
+//  SpriteKitEgetProjekt
+//
+//  Created by Lina Li on 2020-12-14.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitEgetProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
